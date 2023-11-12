a4f07de4281d613ed82a0676ddcafebcd4f69103
Kristofer Berggren <kristofer.berggren@gmail.com>
2023-11-12 13:13:56 +0800

    update curl argument to follow redirect for wrapper script

--- li.sh
+++ li.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-#bash <(curl -s https://raw.githubusercontent.com/d99kris/knixtools/main/bin/livecd-install.sh) "${@}"
-bash <(curl -sL https://raw.githubusercontent.com/d99kris/nmail/master/make.sh) "${@}"
+bash <(curl -sL https://raw.githubusercontent.com/d99kris/knixtools/main/bin/livecd-install.sh) "${@}"
