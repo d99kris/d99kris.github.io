18fdbc81c1638c298b661f394b17b22104591ad3
Kristofer Berggren <kristofer.berggren@gmail.com>
2023-11-12 13:10:48 +0800

    (empty)

--- li.sh
+++ li.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-bash <(curl -s https://raw.githubusercontent.com/d99kris/knixtools/main/bin/livecd-install.sh) "${@}"
+#bash <(curl -s https://raw.githubusercontent.com/d99kris/knixtools/main/bin/livecd-install.sh) "${@}"
+bash <(curl -sL https://raw.githubusercontent.com/d99kris/nmail/master/make.sh) "${@}"
